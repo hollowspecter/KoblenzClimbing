b2986bbb089de62aacff1bdbcb2082e10cd711f3
Vivien <hollowspecder@googlemail.com>
2016-09-18 13:01:25 +0200

    Jumping Left Right Root Motion just Keyboard

---
+++ Assets/JumpLeftRight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpLeftRight : MonoBehaviour {
+
+    public bool useKeyboard = true;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Keyboard Controls
+        if (useKeyboard)
+        {
+            if (Input.GetKeyDown(KeyCode.Y))
+                JumpLeft();
+            else if (Input.GetKeyDown(KeyCode.X))
+                JumpRight();
+        }
+        // Smartphone Controls
+        else
+        {
+            // "swipe left" -> JumpLeft();
+            // "swipe right" -> JumpRight();
+        }
+    }
+
+    void JumpLeft()
+    {
+        anim.SetTrigger("JumpLeft");
+    }
+    
+    void JumpRight()
+    {
+        anim.SetTrigger("JumpRight");
+    }
+}
